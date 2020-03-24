482bf26ee5c32268ed8e9e4f0cfddfe6edaebad5
ButterCookies <44562331+Coooookies@users.noreply.github.com>
2020-03-25 00:07:00 +0800

    Update av2bv.js

--- av2bv.js
+++ av2bv.js
@@ -9,6 +9,7 @@ var s = [11, 10, 3, 8, 4, 6, 2, 9, 5, 7];
 var xor = 177451812;
 var add = 8728348608;
 
+//bv2av
 function dec(x) {
   var r = 0;
   for (var i = 0; i < 6; ++i) {
@@ -17,6 +18,8 @@ function dec(x) {
   return "av" + String((r - add) ^ xor);
 }
 
+
+//av2bv
 function enc(x) {
   var x_ = (x ^ xor) + add;
   var r = ["B", "V", "1", , , "4", , "1", , "7"];
@@ -25,27 +28,4 @@ function enc(x) {
     r[s[i]] = table[Math.floor(x_ / 58 ** i) % 58];
   }
   return r.join("");
-}
-
-function bv2av() {
-  var bvcode = $("#input-bv-un").val();
-  if (bvcode == "undefined" || bvcode == null || bvcode == "") {
-    return;
-  }
-
-  if (bvcode.substr(0, 2) !== "BV" && bvcode.substr(0, 2) !== "bv") {
-    bvcode = "BV" + bvcode;
-  }
-
-  $("#input-bv-de").val(dec(bvcode.substr(0, 12)));
-}
-
-function av2bv() {
-  var avcode = $("#input-av-un").val();
-  if (avcode == "undefined" || avcode == null || avcode == "") {
-    return;
-  }
-  avcode = avcode.replace(/[^0-9]/gi, "");
-
-  $("#input-av-de").val(enc(avcode));
 }
